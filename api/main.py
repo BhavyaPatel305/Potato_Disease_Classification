e941d2beee5b07764e07ceb40c8cdc8e72bb9ab5
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2024-03-13 00:24:50 -0400

    Making predictions for images being uploaded from POSTMAN and returning predicted class and confidence - Success

--- api/main.py
+++ api/main.py
@@ -7,9 +7,19 @@ import numpy as np
 from io import BytesIO
 # Module to read images
 from PIL import Image
+# Import Tensorflow
+import tensorflow as tf
 
 # Create an app/ instance of FAST API
 app = FastAPI()
+
+# Load the saved tensorflow model
+# For now just loading model number 1
+MODEL = tf.keras.models.load_model("../saved_models/1")
+# Class Names need to be consistent with: ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
 
 # ping method to make sure server is alive.
 @app.get("/ping")
@@ -43,7 +53,31 @@ async def predict(
     # To do that read the file to get the bytes
     # bytes = await file.read() (Next: Convert these bytes to numpy array)
     image = read_file_as_image(await file.read()) # Numpy Array
-    return
+    
+    # predict dosen't accept a single image, it has to be a batch image
+    # Meaning: say we have an image: [256, 256, 3]
+    # predict cannot accept just [256, 256, 3]
+    # So we need to convert this image to a batch, [[256, 256, 3]]
+    # Just converting it from 1D to 2D, by this: np.expand_dims(image, 0)
+    image_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(image_batch)
+    # Since we have only 1 image, so take the 1st prediction: prediction[0]
+    # prediction[0]: [0.11, 0.99, 0.25] as we have used softmax function which gives probablities
+    # Compare it with: ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+    # now whichever of 3 has maximum value, that is our prediction: Late Blight (Example)
+    # so by using argmax we get index 0,1, or 2
+    # Pass this index to CLASS_NAMES array, to get  actual class name
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    # Confidence will be 0.99 value in this prediction[0]: [0.11, 0.99, 0.25] array
+    confidence = np.max(predictions[0])
+    
+    # Return predicted_class and confidence as a dictionary
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
+    
+# COMMAND TO RUN  THIS FILE ON LOCAL HOST: uvicorn main:app --reload
